784879a15f4d5dc0bac2f76e6c2536558edcca4e
Sonia Chemaa <sonia.chemaa@outlook.com>
2017-09-06 14:03:57 +0200

    password form created

--- motdepasse.php
+++ motdepasse.php
@@ -1,7 +1,8 @@
 <!-- mot de passe = kangourou -->
 
-<?php
+<p>GET TO THE NASA</p>
 
-
-
- ?>
+  <form action="secret.php" method="post">
+    <input type="password" name="password">
+    <input type="submit" name="send" value="Send Password">
+  </form>
---
+++ secret.php
@@ -0,0 +1,20 @@
+<?php
+
+// if(isset($_POST['password']) AND $_   )
+// {
+//
+//
+//
+//
+//
+// } else {
+//   echo "/!\ Password invalid - Try again /!\ ";
+// }
+
+
+
+
+
+
+
+ ?>
