8f5f720c0b5a15bcd7f5210fce6d3858766c20af
Sonia Chemaa <sonia.chemaa@outlook.com>
2017-09-06 22:27:49 +0200

    php file on one page only

--- motdepasse.php
+++ motdepasse.php
@@ -2,11 +2,22 @@
 
 <p>GET TO THE NASA</p>
 
-  <form action="secret.php" method="post">
-    <p>Pseudo</p></br>
-    <input type="text" name="name" required="required">
-    <p>Password</p></br>
-    <input type="password" name="password">
-    <input type="submit" name="send" value="Send Password">
-  </form>
-  
+  <?php
+  if(!isset($_POST’['password']) AND $_POST['password'] != "kangourou")
+  {
+    echo "Accès refusé";
+    ?>
+
+    <form action="motdepasse.php" method="post">
+      <p>Pseudo</p></br>
+      <input type="text" name="name" required="required">
+      <p>Password</p></br>
+      <input type="password" name="password">
+      <input type="submit" name="send" value="Send Password">
+    </form>
+
+    <?php
+  } else {
+  echo "Accès autorisé pour " . $_POST['name']. ".";
+}
+?>
--- secret.php
+++ secret.php
@@ -1,15 +1,1 @@
-<?php
 
-$validpassword = $_POST['password'];
-$passwordlength = strlen($validpassword);
-
-if(isset($validpassword) AND $passwordlength > 0 AND $validpassword == "kangourou")
-{
-    echo "Bonjour " . $_POST['name']. " Voici les codes d'accès au serveur central de la nasa... </br>";
-    echo "Nan je dékonne!! BIG TROLL BAAAAAAH TROU DUC";
-
-} else {
-  echo "/!\ Password invalid - Try again /!\ ";
-}
-
- ?>
