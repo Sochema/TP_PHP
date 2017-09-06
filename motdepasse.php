8dee1b0a5e9721cc3e118eabad6c3ad10262f7f0
Sonia Chemaa <sonia.chemaa@outlook.com>
2017-09-06 14:43:55 +0200

    password required to acces nasa data

--- motdepasse.php
+++ motdepasse.php
@@ -3,6 +3,9 @@
 <p>GET TO THE NASA</p>
 
   <form action="secret.php" method="post">
+    <p>Pseudo</p></br>
+    <input type="text" name="name" required="required">
+    <p>Password</p></br>
     <input type="password" name="password">
     <input type="submit" name="send" value="Send Password">
   </form>
--- secret.php
+++ secret.php
@@ -1,20 +1,15 @@
 <?php
 
-// if(isset($_POST['password']) AND $_   )
-// {
-//
-//
-//
-//
-//
-// } else {
-//   echo "/!\ Password invalid - Try again /!\ ";
-// }
+$validpassword = $_POST['password'];
+$passwordlength = strlen($validpassword);
 
+if(isset($validpassword) AND $passwordlength > 0 AND $validpassword == "kangourou")
+{
+    echo "Bonjour " . $_POST['name']. " Voici les codes d'accès au serveur central de la nasa... </br>";
+    echo "Nan je dékonne!! BIG TROLL BAAAAAAH TROU DUC";
 
-
-
-
-
+} else {
+  echo "/!\ Password invalid - Try again /!\ ";
+}
 
  ?>
